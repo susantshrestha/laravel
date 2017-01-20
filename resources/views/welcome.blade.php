8f8b77be1c93c063aafcc76d0d6d7100d19d23bc
susantshrestha <susantshrestha@hotmail.com>
2017-01-20 20:23:55 +0545

    second

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
+                <div class="title">@yeild('content')</div>
             </div>
         </div>
     </body>
